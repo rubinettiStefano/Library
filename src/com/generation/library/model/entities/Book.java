e30204333ae24a510e37f14bf65fbbc042ff6969
Valerio Iorio <valexavi1234@gmail.com>
2022-03-31 12:48:55 +0200

    Aggiunto prezzo al toString()

--- src/com/generation/library/model/entities/Book.java
+++ src/com/generation/library/model/entities/Book.java
@@ -16,8 +16,10 @@ public class Book
 
     public String toString()
     {
-        return "Title: "+ title + "\nAuthor: "+ author;
+        return "Title: "+ title + "\nAuthor: "+ author+ "/nPrice: "+ price;
     }
+
+
 
     
 
