bff4f5e92bdd8f2354ed8c1e5698f9441f704613
Valerio Iorio <valexavi1234@gmail.com>
2022-03-31 12:37:35 +0200

    aggiunto costruttore alla classe Book

--- src/com/generation/library/model/entities/Book.java
+++ src/com/generation/library/model/entities/Book.java
@@ -2,6 +2,18 @@ package com.generation.library.model.entities;
 
 public class Book 
 {
+    String title, author;
     int nPages;
-    double price;    
+    double price;
+
+    public Book(String title, String author, int nPages, double price) 
+    {
+        this.title = title;
+        this.author = author;
+        this.nPages = nPages;
+        this.price = price;
+    } 
+
+    
+
 }
